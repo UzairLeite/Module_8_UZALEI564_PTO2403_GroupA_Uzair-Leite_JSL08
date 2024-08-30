d52ae7a4f7469094a9784d038fb331769453dfe0
UzairLeite <102726763+UzairLeite@users.noreply.github.com>
2024-08-30 10:53:58 +0200

    Define class and variable

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
+var bankBranchInstance = null;
 
 // 2. Define a class called `BankBranch` for managing branch information.
-
+class BankBranch {
+    constructor(branchName, branchAddress) {
+        this.branchName = branchName;
+    }
+}
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
